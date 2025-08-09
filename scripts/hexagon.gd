e070691a2821730c8f0db91c91b27c3f63c747c8
Martin Briand <martinbriands@gmail.com>
2025-08-09 08:33:08 -0400

    mapped texture

--- scripts/hexagon.gd
+++ scripts/hexagon.gd
@@ -2,6 +2,22 @@ extends Node3D
 
 class_name Hexagon
 #func _ready() -> void:
+
+var parent_position: Vector3
+
+func set_hexagon(parent: Node3D, pos: Vector3):
+    position = pos
+    look_at(parent.position)
+    
+    parent_position = parent.position
+    
+func apply_bounds(bounds_x: Vector2, bounds_y: Vector2):
+    var x_span = (bounds_x.y - bounds_x.x) / 2
+    var y_span = (bounds_y.y - bounds_y.x) / 2
+    var map_position = Vector2(position.x + x_span, position.y + y_span)
+    
+    set_color(Color(map_position.y, map_position.x, 0, 1))
+    
     
     
 func set_color(color: Color):
--- scripts/icosphere.gd
+++ scripts/icosphere.gd
@@ -2,21 +2,39 @@ extends MeshInstance3D
 
 @export var Plate : PackedScene
 
-func _ready():
-    print("ready")
+var hexagons: Array[Hexagon]
+
+var bounds_x: Vector2 = Vector2(-0.5, 0.5)
+var bounds_y: Vector2 = Vector2(-0.5, 0.5)
+
+func _ready():    
+    
+    var max = 0
+    var min = 0
     
     for face in mesh.get_faces():
-        var b = Plate.instantiate() as Node3D
-        add_child(b)
-        b.position = face
-        b.look_at(position)
-        b.position += randi_range(0, 6) * (b.position - position).normalized() / 50
+        var hexagon = Plate.instantiate() as Node3D
+        add_child(hexagon)
+        #b.position = face
+        #b.look_at(position)
+        #b.position += randi_range(0, 6) * (b.position - position).normalized() / 50
         
         #print(b.position.y)
+                
+        hexagon.set_hexagon(self, face)
+        hexagon.apply_bounds(bounds_x, bounds_y)
         
-        if (b.position.y > 0.5) or (b.position.y < -0.5):
-            print("we should change color")
-            var s = b as Hexagon
-            s.set_color(Color(1,1,1,1))
+        hexagons.append(hexagon)
+        
+        max = max(face.y, max)
+        min = min(face.y, min)
+        
+        #if (b.position.y > 0.5) or (b.position.y < -0.5):
+            #print("we should change color")
+            #var s = b as Hexagon
+            #s.set_color(Color(1,1,1,1))
+    
+    print(max)
+    print(min)
 
         
